fe11228fdce98c06d605fd9624c8b2ffc517e0c2
Nathan Brickett <natey37@gmail.com>
2019-12-19 06:36:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
 def roll_call_dwarves(array)
   # code an argument here
   # Your code here
-  array.each |dwarf|
-    puts dwarf
+  array.each_with_index |index, dwarf|
+    puts "#{index + 1}, #{dwarf}"
   end 
 end
 
