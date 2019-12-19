b185649c71b20370efd74bf8c2cfb2387fbaea96
Nathan Brickett <natey37@gmail.com>
2019-12-19 06:40:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 def summon_captain_planet(array)
   # code an argument here
   # Your code here
-  array.map{|element| "#{element}!" }
+  array.map{|element| "#{element.capatil}!" }
 end
 
 def long_planeteer_calls(array)
